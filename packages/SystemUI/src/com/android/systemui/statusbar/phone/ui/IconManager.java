d24e432b5797789f342d1b157d8b5ed1b4b0f6fd
Steve Kondik <steve@cyngn.com>
2014-12-05 08:31:29 -0800

    SystemUI: Forward-port notification counters
    Squashed:
    
        From: d34d <clark@cyngn.com>
        Date: Mon, 13 Feb 2017 01:12:57 -0500
        Subject: [PATCH 292/732] SystemUI: Show infinity for large notification counts
    
        This patch adds a new string to SystemUI for displaying the infinity
        symbol when a notification has a count larger than 999.  This only
        affects the notification icon in the status bar and does not change
        the text displayed in the actual notification.
    
        These changes were manually cherry-picked from cm-13.0.
    
        Change-Id: I3481aac13cd90ee20ed48194df3ec789c16c8b4b
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: jhonboy121 <alfredmathew05@gmail.com>
        Date: Sun, 2 Jan 2022 18:09:39 +0530
        Subject: SystemUI: Refactor notification counter
    
        * Update icons from NotificationIconAreaController when settings change and set initial state from StatusBarIconController
        * Also use value of config_statusBarShowNumber as the default
    
        [neobuddy89: Add required NPE checks to properly apply settings on boot.]
    
        Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
        From: Fran Palmero <sweetydesigns@gmail.com>
        Date: Thu, 21 Jul 2022 05:05:10 +0200
        Subject: SystemUI: Update notification count overlay
    
        * Updated to A12 style
        * Overlay picked from figma.com/community/plugin/1088610476491668236 edited with GIMP 2.10.18
    
        Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    [@neobuddy89: Updated and adapted for A14 QPR1, Restored and reworked for A15 since Google deleted it]
    
    Change-Id: Ia8609c56a523906404a9155cea6987f40c10f30d
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/IconManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ui/IconManager.java
@@ -113,6 +113,7 @@ public class IconManager implements DemoModeCommandReceiver {
     protected ArrayList<String> mBlockList = new ArrayList<>();
 
     private final boolean mNewIconStyle;
+    private final boolean mShowNotificationCount;
 
     public IconManager(
             ViewGroup group,
@@ -133,6 +134,9 @@ public class IconManager implements DemoModeCommandReceiver {
 
         mNewIconStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
             Settings.System.STATUSBAR_COLORED_ICONS, 0, UserHandle.USER_CURRENT) == 1;
+        mShowNotificationCount = Settings.System.getIntForUser(mContext.getContentResolver(),
+            Settings.System.STATUSBAR_NOTIF_COUNT, 0,
+            UserHandle.USER_CURRENT) == 1;
 
         reloadDimens();
 
@@ -204,6 +208,7 @@ public class IconManager implements DemoModeCommandReceiver {
             StatusBarIcon icon) {
         StatusBarIconView view = onCreateStatusBarIconView(slot, blocked);
         view.setIconStyle(mNewIconStyle);
+        view.setShowCount(mShowNotificationCount);
         view.set(icon);
         mGroup.addView(view, index, onCreateLayoutParams(icon.shape));
         return view;
