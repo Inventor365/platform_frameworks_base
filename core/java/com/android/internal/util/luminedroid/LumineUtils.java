e1aa7bd5a56188d088d74179977d0e1303b9f6fb
Pranav Vashi <neobuddy89@gmail.com>
2025-04-14 21:52:56 +0530

    base: Add method to restart SystemUI
    * Killing com.android.systemui using IActivityManager doesnt work always.
    * Instead use CommandQueue callback to make SystemUI kill itself. Works!
    
    Change-Id: Iba209b54546700ba4c51d39613415b0279582350
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/com/android/internal/util/luminedroid/LumineUtils.java
+++ core/java/com/android/internal/util/luminedroid/LumineUtils.java
@@ -264,4 +264,13 @@ public class LumineUtils {
         }
         return NO_CUTOUT;
     }
+
+    public static void restartSystemUI() {
+        final IStatusBarService mBarService = IStatusBarService.Stub.asInterface(
+                ServiceManager.getService(Context.STATUS_BAR_SERVICE));
+        try {
+            mBarService.restartSystemUI();
+        } catch (RemoteException e) {
+        }
+    }
 }
