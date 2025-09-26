43b1798628f4e8d1e9cf94d6f49649137dc72397
chandu078 <chandudyavanapelli03@gmail.com>
2025-09-26 13:21:34 +0000

    Revert "SystemUI: Remove composite alpha from additional scrim on lockscreen"
    This reverts commit 9b1e7babd6b1c07fc0fb586405b8c16d661933a0.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -201,6 +201,10 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
     }
 
     /**
+     * Default alpha value for most scrims.
+     */
+    protected static final float KEYGUARD_SCRIM_ALPHA = 0.2f;
+    /**
      * Scrim opacity when the phone is about to wake-up.
      */
     public static final float WAKE_SENSOR_SCRIM_ALPHA = 0.6f;
@@ -247,7 +251,7 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
 
     private float mAdditionalScrimBehindAlphaKeyguard = 0f;
     // Combined scrim behind keyguard alpha of default scrim + additional scrim
-    private float mScrimBehindAlphaKeyguard = 0f;
+    private float mScrimBehindAlphaKeyguard = KEYGUARD_SCRIM_ALPHA;
 
     static final float TRANSPARENT_BOUNCER_SCRIM_ALPHA = 0.54f;
 
@@ -709,7 +713,11 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
      * This is used to apply additional keyguard dimming on top of the default scrim alpha value.
      */
     protected void applyCompositeAlphaOnScrimBehindKeyguard() {
-        setScrimBehindValues(mAdditionalScrimBehindAlphaKeyguard);
+        int compositeAlpha = ColorUtils.compositeAlpha(
+                (int) (255 * mAdditionalScrimBehindAlphaKeyguard),
+                (int) (255 * KEYGUARD_SCRIM_ALPHA));
+        float keyguardScrimAlpha = (float) compositeAlpha / 255;
+        setScrimBehindValues(keyguardScrimAlpha);
     }
 
     /**
@@ -1386,13 +1394,17 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
         if (mAnimatorListener != null) {
             anim.addListener(mAnimatorListener);
         }
+        final int initialScrimTint = scrim instanceof ScrimView ? ((ScrimView) scrim).getTint() :
+                Color.TRANSPARENT;
         anim.addUpdateListener(animation -> {
             final float startAlpha = (Float) scrim.getTag(TAG_START_ALPHA);
             final float animAmount = (float) animation.getAnimatedValue();
+            final int finalScrimTint = getCurrentScrimTint(scrim);
             final float finalScrimAlpha = getCurrentScrimAlpha(scrim);
             float alpha = MathUtils.lerp(startAlpha, finalScrimAlpha, animAmount);
             alpha = MathUtils.constrain(alpha, 0f, 1f);
-            updateScrimColor(scrim, alpha, (mState == ScrimState.KEYGUARD ? Color.BLACK : Color.TRANSPARENT));
+            int tint = ColorUtils.blendARGB(initialScrimTint, finalScrimTint, animAmount);
+            updateScrimColor(scrim, alpha, tint);
             dispatchScrimsVisible();
         });
         anim.setInterpolator(mInterpolator);
