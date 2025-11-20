Twinchin12/android_frameworks_base-2

Do not backup APKs that do not have `allowBackup` attr on it's manifests
* the default bool() function was returning the default value if the attr is null, this results to parsingpackageutils setting allowBackup to true for our theme overlays, causing the parcel size to grow above the current binder size limit, which results to binder transaction failure when BackupTransport utilizes the binder.

reference:
https://github.com/Evolution-X/frameworks_base/commit/92693d71a12d53e8cd09728b1671cb5269c41276

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

services/core/java/com/android/server/pm/pkg/parsing/ParsingPackageUtils.java
@@ -2236,7 +2236,7 @@ public class ParsingPackageUtils {
         // CHECKSTYLE:off
         pkg
                 // Default true
-                .setAllowBackup(bool(true, R.styleable.AndroidManifestApplication_allowBackup, sa))
+                .setAllowBackup(bool(sa == null ? false : true, R.styleable.AndroidManifestApplication_allowBackup, sa))
                 .setAllowClearUserData(bool(true, R.styleable.AndroidManifestApplication_allowClearUserData, sa))
                 .setAllowClearUserDataOnFailedRestore(bool(true, R.styleable.AndroidManifestApplication_allowClearUserDataOnFailedRestore, sa))
                 .setAllowNativeHeapPointerTagging(bool(true, R.styleable.AndroidManifestApplication_allowNativeHeapPointerTagging, sa))

Inventor365/platform_frameworks_base

core/java/com/android/internal/pm/pkg/parsing/ParsingPackageUtils.java
@@ -2392,7 +2392,7 @@ public class ParsingPackageUtils {
         // CHECKSTYLE:off
         pkg
                 // Default true
-                .setBackupAllowed(bool(true, R.styleable.AndroidManifestApplication_allowBackup, sa))
+                .setBackupAllowed(bool(sa == null ? false : true, R.styleable.AndroidManifestApplication_allowBackup, sa))
                 .setClearUserDataAllowed(bool(true, R.styleable.AndroidManifestApplication_allowClearUserData, sa))
                 .setClearUserDataOnFailedRestoreAllowed(bool(true, R.styleable.AndroidManifestApplication_allowClearUserDataOnFailedRestore, sa))
                 .setAllowNativeHeapPointerTagging(bool(true, R.styleable.AndroidManifestApplication_allowNativeHeapPointerTagging, sa))