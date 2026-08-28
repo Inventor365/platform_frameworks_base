535cb0bfd98399c46348443d292fe88c0cf3f2e6
Inventor365 <89648048+Inventor365@users.noreply.github.com>
2026-08-28 04:37:31 +0000

    SystemUI: Implement native PowerProfileTile & PowerProfileDialog with DialogTransitionAnimator
    Migrate Power Profile QS selector panel to SystemUI-owned architecture using DialogTransitionAnimator (matching Pixel InternetDialog / ProfilesTile).
    
    - Launch panel directly from originating QS tile view using DialogTransitionAnimator.show(dialog, controller)
    - Keep QS shade expanded behind the floating selector panel when opened and dismissed
    - Retain interactive bottom-left HTSR toggle and bottom-right Done button
    - Keep panel open on profile selection for continuous interaction
    - Fix intermittent disappearance bug by removing external TileService IPC and window token teardown

--- packages/SystemUI/src/com/android/systemui/qs/external/TileServices.java
+++ packages/SystemUI/src/com/android/systemui/qs/external/TileServices.java
@@ -83,6 +83,7 @@ public class TileServices extends IQSService.Stub {
     private final TileLifecycleManager.Factory mTileLifecycleManagerFactory;
     private final CustomTileAddedRepository mCustomTileAddedRepository;
     private final DelayableExecutor mBackgroundExecutor;
+    private final com.android.systemui.animation.DialogTransitionAnimator mDialogTransitionAnimator;
 
     private int mMaxBound = DEFAULT_MAX_BOUND;
 
@@ -98,7 +99,8 @@ public class TileServices extends IQSService.Stub {
             PanelInteractor panelInteractor,
             TileLifecycleManager.Factory tileLifecycleManagerFactory,
             CustomTileAddedRepository customTileAddedRepository,
-            @Background DelayableExecutor backgroundExecutor) {
+            @Background DelayableExecutor backgroundExecutor,
+            com.android.systemui.animation.DialogTransitionAnimator dialogTransitionAnimator) {
         mHost = host;
         mKeyguardStateController = keyguardStateController;
         mContext = mHost.getContext();
@@ -113,6 +115,11 @@ public class TileServices extends IQSService.Stub {
         mTileLifecycleManagerFactory = tileLifecycleManagerFactory;
         mCustomTileAddedRepository = customTileAddedRepository;
         mBackgroundExecutor = backgroundExecutor;
+        mDialogTransitionAnimator = dialogTransitionAnimator;
+    }
+
+    public com.android.systemui.animation.DialogTransitionAnimator getDialogTransitionAnimator() {
+        return mDialogTransitionAnimator;
     }
 
     public Context getContext() {
